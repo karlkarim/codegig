3fec17d19c576b3c2ccfb7fea74129262ca3f27b
karlkarim <karl.karimoisa@tmd.ee>
2020-05-08 17:06:07 +0300

    after first video

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const db = require('./config/database')
+
+
+
+db.authenticate()
+.then(() => console.log('Database connected'))
+.catch(err => console.log('Error'+ err))
+
+const app = express();
+
+app.get('/', (req,res) => res.send('INDEX'));
+
+app.use('/gigs', require('./routes/gigs'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
